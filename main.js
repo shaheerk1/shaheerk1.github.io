060ad69f56a24d5476fd0b014eaefeaf29410e0b
shahirk48 <42793050+shahirk48@users.noreply.github.com>
2020-11-23 12:44:01 +0530

    chnged-repo-to-portfoliosite

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+const menu = document.getElementById('ham');
+const navbar = document.getElementById('nav');
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+});
+
+
+
